048c7a56eda6dfe0151b21c4915ac6211e07001a
Deepanshu291 <73581139+Deepanshu291@users.noreply.github.com>
2020-11-12 20:24:27 +0530

    2nd

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+//Scroll Navbar 
+
 window.addEventListener("scroll", function () {
     //var h = document.querySelector("nav").height();
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 10 );
 })
 
+//Mobile Navbar 
+
+const menu = document.querySelector("ul");
+
+    function menuclick() {
+    menu.classList.add("active");
+    }
+    function Cancel() {
+        menu.classList.remove("active");
+    }
